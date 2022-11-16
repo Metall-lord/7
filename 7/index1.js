ccdb423a512bc4a2244369f748ec635cb5b69125
Антаков Константин <116781143+Metall-lord@users.noreply.github.com>
2022-11-16 13:02:39 +0700

    Add files via upload

--- 7/index1.js
+++ 7/index1.js
@@ -1,9 +1,9 @@
-function cart(basketAmount, countGoods, promo = null) { let totalAmount = basketAmount;
-if (promo = 'Промокод: ДАРИМ300' && basketAmount < 300) {totalAmount = 0; console.log('Промокод: ДАРИМ300 ' + totalAmount + '₽');}
-else if (promo = 'Прокод: ДАРИМ300')
-{totalAmount -= 300; console.log('ДАРИМ300 ' + totalAmount + '₽');}
-if (countGoods >= 10) {const countFiveProcent = basketAmount/100*5; totalAmount -= countFiveProcent; console.log('Более 10 товаров ' + totalAmount + '₽');}
-if (basketAmount > 50000) {const countTwentyProcent = totalAmount/100*20; const countDifference = totalAmount - 50000; totalAmount = countDifference - countTwentyProcent; console.log('Более 50 000 ₽ ' + totalAmount + '₽');}
-if (promo = 'Акция: СКИДКА15' && basketAmount >= 20000) {const countFifteenProcent = totalAmount/100*15; totalAmount -= countFifteenProcent; console.log('Акция: СКИДКА15 ' + totalAmount +'₽'); }
-console.log('Стоимость товаров равна ' + totalAmount + '₽');}
+function cart(KarzinaStoimost, PodschetHorosho, Talonchik = null) { let ObshayaStoimost = KarzinaStoimost;
+if (Talonchik = 'Промокод: ДАРИМ300' && KarzinaStoimost < 300) {ObshayaStoimost = 0; console.log('Промокод: ДАРИМ300 ' + ObshayaStoimost + '₽');}
+else if (Talonchik = 'Прокод: ДАРИМ300')
+{ObshayaStoimost -= 300; console.log('ДАРИМ300: ' + ObshayaStoimost + '₽');}
+if (PodschetHorosho >= 10) {const PodschetFiveProcent = KarzinaStoimost/100*5; ObshayaStoimost -= PodschetFiveProcent; console.log('Более 10 товаров: ' + ObshayaStoimost + '₽');}
+if (KarzinaStoimost > 50000) {const PodschetTwentyProcent = ObshayaStoimost/100*20; const PodschetDifference = ObshayaStoimost - 50000; ObshayaStoimost = PodschetDifference - PodschetTwentyProcent; console.log('Более 50 000 ₽: ' + ObshayaStoimost + '₽');}
+if (Talonchik = 'Акция: СКИДКА15' && KarzinaStoimost >= 20000) {const PodschetPedesaytProcent = ObshayaStoimost/100*15; ObshayaStoimost -= PodschetPedesaytProcent; console.log('Акция: СКИДКА15 ' + ObshayaStoimost +'₽'); }
+console.log('Стоимость товаров равна: ' + ObshayaStoimost + '₽');}
 cart(100000, 12, 'Промокод: ДАРИМ300');
